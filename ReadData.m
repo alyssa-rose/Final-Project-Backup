012b9a2fcd06a6f30c55085e1db2cc64ddb58af9
Sid Hillwig <sidneyhillwig@gmail.com>
2018-04-29 16:28:54 -0400

    Add files via upload

--- ReadData.m
+++ ReadData.m
@@ -1,12 +1,12 @@
 % script to read in food data
-T = readtable('dataFinal-v.3.xlsx','Range','B1:W396');
+T = readtable('dataFinal-v.3.xlsx','Range','B1:W397');
 %% forming structures
-areaCodes = unique(T{:,1});
-foodGroups = T{:,2}(1:11);
-areaNames = unique(T{:,3});
-lat = unique(T{:,4});
-long = unique(T{:,5});
-foodData = T{:,6:end};
+areaCodes = unique(T{:,1}); % is an array
+foodGroups = T{:,2}(1:end); % is a cell
+areaNames = unique(T{:,3}); % is a cell
+lat = unique(T{:,4}); % is an array
+long = unique(T{:,5}); % is an array
+foodData = T{:,6:end}; % is an array
 
 for j = (1:length(areaNames(:)))
    startIn = ((areaCodes(j)-1)*10) + areaCodes(j);
