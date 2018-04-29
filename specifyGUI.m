b79a9be3247cce3d10f987f9049f93835f809e7e
PugOfTheNebulae <32802037+PugOfTheNebulae@users.noreply.github.com>
2018-04-29 10:00:21 -0400

    Update specifyGUI.m

--- specifyGUI.m
+++ specifyGUI.m
@@ -1,21 +1,21 @@
 function specifyGUI
-
+s = input('enter country: ','s')
 figure
 hold on
 axesm globe;
-% gridm('GLineStyle','-','Gcolor',[.7 .8 .9],'Grid','on')
 gridm('GLineWidth',0.75,'Grid','on')
-set(gca,'Box','off','Projection','perspective')
-base = zeros(180,360);
-baseR = georefcells([-90 90],[0 360],size(base));
-copperColor = [0.62 0.38 0.24];
-hs = geoshow(base,baseR,'FaceColor',copperColor);
 
+% popup = uicontrol('Style', 'popup',...
+%     'String',{'North America','South America','Europe', 'Africa','Asia'}...
+%     ,'Callback',@setmap);
+%     hold off
+
+worldmap(s)
+load coastlines
+
+plotm(coastlat,coastlon)
 setm(gca,'Galtitude',0.025);
 axis vis3d
-
-clmo(hs)
-
 load topo
 topo = topo / (earthRadius('km')* 20);
 hs = meshm(topo,topolegend,size(topo),topo);
@@ -30,8 +30,4 @@ material ([.7, .9, .8])
 
 locAFG = csvread('dataFinal.csv',1,8,'I2..J2')
 plot3m(locAFG(1),locAFG(2),0.1,'m*')
-
-
-popup = uicontrol('Style', 'popup',...
-    'String',{'North America','South America','Europe', 'Africa','Asia'})
-    hold off
+  
