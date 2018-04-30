22d521871b6152ad070ef2f018f8bc0f4a47ee6d
Sid Hillwig <sidneyhillwig@gmail.com>
2018-04-30 10:24:58 -0400

    Add files via upload

--- ClusterDiets.m
+++ ClusterDiets.m
@@ -5,15 +5,25 @@ Par.nClus = 7;
 Par.maxIter = 100;
 %% Carbohydrate, Protein, Lipid Clustering
 areaCPL = cell2mat(struct2cell(CreateGroups(area,'cpl'))); % creates groups for carbs, proteins, lipids
-[ClusInfoCPL,~] = KmeansWrapper(areaCPL,Par,20);
+[ClusInfoCPL,~] = KmeansWrapper(areaCPL,Par,5);
 
 labelCPL = struct('title','Carb-Protein-Lipid Diet Analysis','x','Carbohydrates'...
     ,'y','Proteins','z','Lipids');
 Plot3DClusters(areaCPL,ClusInfoCPL,2,labelCPL)
 
 %% Beverage Clustering
-areaBev = cell2mat(struct2cell(CreateGroups(area,'bev'))); % creates groups for beverages
-[ClusInfoBev,~] = KmeansWrapper(areaBev,Par,20);
+areaBev = CreateGroups(area,'bev');
+bev = areaBev.alcohol; % all the same length, choice of beverage arbitrary
+invalid = zeros(1,length(bev)); 
+for k = (1:length(area)) % looping through all areas
+    if areaBev(k).alcohol == invalid | areaBev(k).coffee == invalid | areaBev(k).tea == invalid % checking if row is all zeros
+        areaBev(k).alcohol = [];
+        areaBev(k).coffee = [];
+        areaBev(k).tea = [];
+    end
+end
+areaBev = cell2mat(struct2cell(areaBev)); % creates groups for beverages
+[ClusInfoBev,~] = KmeansWrapper(areaBev,Par,5);
 
 labelBev = struct('title','Beverage Diet Analysis','x','Alcohol'...
     ,'y','Coffee','z','Tea');
