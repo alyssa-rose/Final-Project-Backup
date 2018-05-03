9b7d64499bee97a5fff13249c4821a24d7138da8
PugOfTheNebulae <32802037+PugOfTheNebulae@users.noreply.github.com>
2018-05-03 18:34:57 -0400

    Create CorrectPoly.m

---
+++ CorrectPoly.m
@@ -0,0 +1,27 @@
+function [polyOrder,yFit,rSq] = CorrectPoly(x,y)
+%{
+function [polyOrder,yFit,rSq] = CorrectPoly(x,y)
+uses matrix math to determine the correct poly order
+to use for a data fit
+
+Alyssa Rose  Final Project 04-29-18
+%}
+
+rSq = zeros(1,5);
+yAvg = mean(y);
+ssTot = sum((y - yAvg).^2);
+for k = 1:5
+    z = k + 1;
+    A = zeros(length(x),z);
+    A(:,(1:z)) = x.^(k:-1:0);
+    condNum = cond(A);
+    coeff1 = A\y;
+    yFit(:,k) = A*coeff1;
+    ssRes = sum((y - yFit(:,k)).^2);
+    rSq(k) = [1-(ssRes./ssTot)];
+end
+[m,I] = max(rSq);
+rSq = m;
+polyOrder = I;
+yFit = yFit(:,I);
+return
